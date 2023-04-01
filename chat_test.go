3cecce5b7b866daf90b46cdb5024021f36dd9894
Gregory Vincic <gregoryvse@gmail.com>
2023-04-01 21:32:25 +0200

    Test chat command

--- chat_test.go
+++ chat_test.go
@@ -1,19 +1,34 @@
 package main
 
 import (
-	"strings"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
 func TestChat(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "{}")
+	}))
+	defer s.Close()
+	u, _ := url.Parse(s.URL)
+
 	c := NewChat()
 
-	if _, err := c.MakeRequest(); err != nil {
+	r, err := c.MakeRequest()
+	if err != nil {
 		t.Error(err)
 	}
+	r.URL.Scheme = u.Scheme
+	r.URL.Host = u.Host
 
-	empty := strings.NewReader("{}")
-	if err := c.HandleResponse(empty); err == nil {
+	body, err := sendRequest(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := c.HandleResponse(body); err == nil {
 		t.Error("empty result should result in error")
 	}
 }
