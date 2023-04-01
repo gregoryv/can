ec9345c78894c3b5149151c0ba5062ba456212b2
Gregory Vincic <gregoryvse@gmail.com>
2023-04-01 22:05:37 +0200

    Test Edits.HandleResponse

--- chat_test.go
+++ chat_test.go
@@ -1,34 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"net/url"
+	"strings"
 	"testing"
 )
 
 func TestChat(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "{}")
-	}))
-	defer s.Close()
-	u, _ := url.Parse(s.URL)
-
 	c := NewChat()
-
-	r, err := c.MakeRequest()
+	_, err := c.MakeRequest()
 	if err != nil {
 		t.Error(err)
 	}
-	r.URL.Scheme = u.Scheme
-	r.URL.Host = u.Host
 
-	body, err := sendRequest(r)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := c.HandleResponse(body); err == nil {
+	empty := strings.NewReader("{}")
+	if err := c.HandleResponse(empty); err == nil {
 		t.Error("empty result should result in error")
 	}
 }
--- edits_test.go
+++ edits_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
@@ -12,8 +14,47 @@ func TestEdits(t *testing.T) {
 		t.Error(err)
 	}
 
-	empty := strings.NewReader("{}")
-	if err := c.HandleResponse(empty); err == nil {
-		t.Error("empty result should result in error")
+	if err := c.HandleResponse(strings.NewReader("{}")); err == nil {
+		t.Error("empty result should fail")
+	}
+
+	if err := c.HandleResponse(strings.NewReader(valid)); err != nil {
+		t.Error(err)
+	}
+
+	// invalid json
+	if err := c.HandleResponse(strings.NewReader("{")); err == nil {
+		t.Error("expect error on invalid json")
+	}
+
+	// check result is written to file
+	dst := filepath.Join(os.TempDir(), "edits.txt")
+	os.WriteFile(dst, []byte(""), 0644)
+	c.Src = dst
+	c.UpdateSrc = true
+	if err := c.HandleResponse(strings.NewReader(valid)); err != nil {
+		t.Fatal(err)
+	}
+	got, _ := os.ReadFile(dst)
+	if v := string(got); v != "word" {
+		t.Errorf("got %q", v)
 	}
 }
+
+// from https://platform.openai.com/docs/api-reference/edits/create
+const valid = `{
+  "object": "edit",
+  "created": 1589478378,
+  "choices": [
+    {
+      "text": "word",
+      "index": 0
+    }
+  ],
+  "usage": {
+    "prompt_tokens": 25,
+    "completion_tokens": 32,
+    "total_tokens": 57
+  }
+}
+`
