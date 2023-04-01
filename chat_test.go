6bdc1373a2f11c626e885c19c7ee9ee0baea958f
Gregory Vincic <gregoryvse@gmail.com>
2023-04-01 20:45:15 +0200

    Test handleResponse for empty

--- chat_test.go
+++ chat_test.go
@@ -1,11 +1,19 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestChat(t *testing.T) {
 	c := NewChat()
 
 	if _, err := c.makeRequest(); err != nil {
 		t.Error(err)
+	}
+
+	empty := strings.NewReader("{}")
+	if err := c.handleResponse(empty); err == nil {
+		t.Error("empty result should result in error")
 	}
 }
--- edits_test.go
+++ edits_test.go
@@ -1,11 +1,19 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestEdits(t *testing.T) {
 	c := NewEdits()
 
 	if _, err := c.makeRequest(); err != nil {
 		t.Error(err)
+	}
+
+	empty := strings.NewReader("{}")
+	if err := c.handleResponse(empty); err == nil {
+		t.Error("empty result should result in error")
 	}
 }
